5c1626d77af0ebd0cb6fd4c29a17550339568261
ZacProvostMcit <202220868@student.montrealcollege.ca>
2026-09-03 19:21:19 -0400

    WelcomeToMcit

---
+++ input.py
@@ -0,0 +1,8 @@
+firstname=input("What is your first name: ")
+
+lastname=input("What is your last name: ")
+
+favoritefood=input("What is your favorite food: ")
+
+print("Welcome to MCIT "+firstname+" "+lastname+" "+"and your favorite food is" +" "+favoritefood)
+
