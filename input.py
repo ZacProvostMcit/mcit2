e0197e66e06d2a58a9eb4d13b99d8f3663d29df7
ZacProvostMcit <202220868@student.montrealcollege.ca>
2026-09-03 19:27:58 -0400

    favorite destination addition

--- input.py
+++ input.py
@@ -4,5 +4,7 @@ lastname=input("What is your last name: ")
 
 favoritefood=input("What is your favorite food: ")
 
-print("Welcome to MCIT "+firstname+" "+lastname+" "+"and your favorite food is" +" "+favoritefood)
+favoritedestination=input("What is your favorite destination: ")
+
+print("Welcome to MCIT "+firstname+" "+lastname+" "+"and your favorite food is" +" "+favoritefood +" "+"and your favorite destiantion is "+favoritedestination)
 
